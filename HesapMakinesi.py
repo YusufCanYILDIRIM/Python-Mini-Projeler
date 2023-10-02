740a94957b1bcd37c8a1d87aa885e8758437d23d
Coders Club <hi@codersclub.com>
2023-10-02 21:03:32 +0300

    kodların ilk kısmı halledildi

---
+++ HesapMakinesi.py
@@ -0,0 +1,17 @@
+sayi1 = int(input("birinci sayi giriniz :"))
+sayi2 = int(input("ikinci sayiyi giriniz :"))
+
+toplama = (sayi1 + sayi2)
+cıkarma = (sayi1 - sayi2)
+carpma = (sayi1 * sayi2)
+bolme = (sayi1 / sayi2)
+
+
+if toplama == "+":
+    print(toplama)
+if cıkarma == "-":
+    print(cıkarma)
+if carpma == "*":
+    print(carpma)
+if bolme == "/":
+    print(bolme)
