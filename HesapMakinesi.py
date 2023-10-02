abcdfc1906e2fbbea518269566512c2f48b1d2d7
Coders Club <hi@codersclub.com>
2023-10-02 21:11:39 +0300

    kodlar düzenlendi

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -1,17 +1,37 @@
 sayi1 = int(input("birinci sayi giriniz :"))
 sayi2 = int(input("ikinci sayiyi giriniz :"))
 
-toplama = (sayi1 + sayi2)
-cıkarma = (sayi1 - sayi2)
-carpma = (sayi1 * sayi2)
-bolme = (sayi1 / sayi2)
+#toplama
+def toplama(sayi1 + sayi2):
+    return sayi1+sayi2
 
+#cıkarma
+def cıkarma(sayi1 - sayi2):
+    return sayi1-sayi2
 
-if toplama == "+":
+#carpma
+def carpma(sayi1 * sayi2):
+    return sayi1*sayi2
+
+#bolme
+def bolme(sayi1 / sayi2):
+    return sayi1/sayi2
+
+print("Hangi işlemi yaptırmak istiyosunuz :")
+print("1- Toplama")
+print("2- Cıkarma")
+print("3- Carpma")
+print("4- Bolme")
+
+secim =(input("Seçiminiz :"))
+
+if toplama == "1":
     print(toplama)
-if cıkarma == "-":
+if cıkarma == "2":
     print(cıkarma)
-if carpma == "*":
+if carpma == "3":
     print(carpma)
-if bolme == "/":
+if bolme == "4":
     print(bolme)
+else:
+    print("Geçersiz giriş")
